26c75beebc7b3025deb4bf19075952429a93f715
Subhendu Sekhar Sahoo <subhendu.sekhar@gmail.com>
2016-09-02 02:57:58 +0530

    #5 Remove Parent activity and nested structure

--- app/src/androidTest/java/com/sekhar/android/calculator/TextInsertionTest.java
+++ app/src/androidTest/java/com/sekhar/android/calculator/TextInsertionTest.java
@@ -24,8 +24,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @LargeTest
 public class TextInsertionTest {
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-            MainActivity.class);
+    public ActivityTestRule<BasicCalculator> mActivityRule = new ActivityTestRule<>(
+            BasicCalculator.class);
 
     @Before
     public void setUp() {}
